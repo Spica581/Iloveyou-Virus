47c8959487ac8d51e1595ddcd480d489c735631f
Sebastian Nicholas B. Ballesteros <sebastianballesteros127@gmail.com>
2026-02-12 15:21:54 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
     proxy: {
       '/api': 'http://localhost:8000'
     }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173
   }
 });
